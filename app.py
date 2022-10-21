17a8ef626c7b71d3e6d5cc884b6b37e89813681a
lijialiang <614237720@qq.com>
2022-10-21 18:42:07 +0800

    fix

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from api import app
+# from api import app
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask Github!'
+
+
+
+
+
 
 
 if __name__ == '__main__':
